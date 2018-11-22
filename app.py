442f14724062b466833e87e56eba3b751232ec1b
jack <jackwgoulart@gmail.com>
2018-11-22 03:13:14 -0300

    fix errors on config files

--- app.py
+++ app.py
@@ -15,9 +15,6 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity) #/auth
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 @app.route('/')
 def home():
@@ -32,4 +29,4 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(host='0.0.0.0',port=5000, debug=True)
+    app.run(port=5000, debug=True)
