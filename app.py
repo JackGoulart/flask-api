199ae01881bfa5ecd5cb3ebb9abe5d6fde19d37c
jack <jackwgoulart@gmail.com>
2018-11-22 04:09:07 -0300

    add users list

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import  Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, UserList
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -23,6 +23,7 @@ def home():
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
+api.add_resource(UserList, '/users')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(UserRegister, '/register')
 
